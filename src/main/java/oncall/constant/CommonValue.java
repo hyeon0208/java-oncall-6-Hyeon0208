8a01fb95fb6488ac7d4e09e4ce23040b8a3f7b28
Hyeon0208 <dmsgktn0208@gmail.com>
2023-12-16 14:34:18 +0900

    feat : 공통 상수를 갖는 클래스 추가

---
+++ src/main/java/oncall/constant/CommonValue.java
@@ -0,0 +1,9 @@
+package oncall.constant;
+
+public class CommonValue {
+    public static final int MONTH_INDEX = 0;
+    public static final int DAY_OF_WEEK_INDEX = 1;
+
+    private CommonValue() {
+    }
+}
