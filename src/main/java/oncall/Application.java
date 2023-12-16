c27bd704fcdc6b083063852247375bf27614469e
Hyeon0208 <dmsgktn0208@gmail.com>
2023-12-16 14:36:37 +0900

    feat : 컨트롤러 실행 기능 추가

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,22 @@
 package oncall;
 
+import oncall.controller.WorkController;
+import oncall.view.ErrorView;
+import oncall.view.InputView;
+import oncall.view.OutputView;
+import oncall.view.handler.InputHandler;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputHandler inputHandler = initHandler();
+        OutputView outputView = new OutputView();
+        WorkController controller = new WorkController(inputHandler, outputView);
+        controller.start();
+    }
+
+    private static InputHandler initHandler() {
+        InputView inputView = new InputView();
+        ErrorView errorView = new ErrorView();
+        return new InputHandler(inputView, errorView);
     }
 }
