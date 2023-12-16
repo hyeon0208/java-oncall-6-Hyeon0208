7e8437d918ced543318e94c989a9d10b57092a55
Hyeon0208 <dmsgktn0208@gmail.com>
2023-12-16 14:12:51 +0900

    feat : 문자열 정수 변환 기능 추가

---
+++ src/main/java/oncall/util/StringConvertor.java
@@ -0,0 +1,11 @@
+package oncall.util;
+
+public class StringConvertor {
+
+    private StringConvertor() {
+    }
+
+    public static int convertToInt(String value) {
+        return Integer.parseInt(value);
+    }
+}
