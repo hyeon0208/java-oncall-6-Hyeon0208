f3c4ce083f92c991fe12dddf8ed84ead6ac955d9
Hyeon0208 <dmsgktn0208@gmail.com>
2023-12-16 14:13:37 +0900

    feat : 닉네임 입력 조건 정규식 추가

--- src/main/java/oncall/view/validator/RegexPattern.java
+++ src/main/java/oncall/view/validator/RegexPattern.java
@@ -3,7 +3,8 @@ package oncall.view.validator;
 import java.util.regex.Pattern;
 
 public enum RegexPattern {
-    ONLY_NUMBER(Pattern.compile("^[0-9]*$"));
+    ONLY_NUMBER(Pattern.compile("^[0-9]*$")),
+    NAME_CONDITION(Pattern.compile("^[a-zA-Z가-힣]*$"));
 
     private final Pattern pattern;
 
