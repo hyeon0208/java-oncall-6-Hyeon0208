b5bd9245e50cef72f7eeeb849c9312eddc7b0e62
Hyeon0208 <dmsgktn0208@gmail.com>
2023-12-16 17:45:50 +0900

    refactor : 메서드라인이 15라인 넘지 않도록 수정

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -1,10 +1,16 @@
 package oncall.view;
 
-import java.time.LocalDate;
 import oncall.domain.Employees;
 import oncall.domain.WorkDay;
 
 public class OutputView {
+    private int weekdayIndex;
+    private int weekendIndex;
+
+    public OutputView() {
+        this.weekdayIndex = 0;
+        this.weekendIndex = 0;
+    }
 
     public void printStartWorkDayInputMessage() {
         System.out.print("비상 근무를 배정할 월과 시작 요일을 입력하세요> ");
@@ -19,27 +25,37 @@ public class OutputView {
     }
 
     public void printWorkSchedule(WorkDay workDay, Employees employees) {
-        int weekdayIndex = 0;
-        int weekendIndex = 0;
         for (int i = 0; i < employees.getEmployeeCount(); i++) {
-            LocalDate date = workDay.getDate();
             if (workDay.isWeekend()) {
-                System.out.printf("%d월 %d일 %s %s\n", date.getMonthValue(), date.getDayOfMonth(), workDay.getDayOfWeekByKorean(),
-                        employees.getWeekendEmployeeNameBy(weekendIndex));
-                weekendIndex++;
+                printWeekendScheduleFormat(workDay ,employees.getWeekendEmployeeNameBy(weekendIndex));
             }
             if (workDay.isWeekDay()) {
-                System.out.printf("%d월 %d일 %s %s\n", date.getMonthValue(), date.getDayOfMonth(), workDay.getDayOfWeekByKorean(),
-                        employees.getWeekdayEmployeeNameBy(weekdayIndex));
-                weekdayIndex++;
+                printWeekDayScheduleFormat(workDay ,employees.getWeekdayEmployeeNameBy(weekdayIndex));
             }
             if (workDay.isHoliday()) {
-                System.out.printf("%d월 %d일 %s (휴일) %s\n", date.getMonthValue(), date.getDayOfMonth(), workDay.getDayOfWeekByKorean(),
-                        employees.getWeekendEmployeeNameBy(weekendIndex));
-                weekendIndex++;
+                printHolidayScheduleFormat(workDay, employees.getWeekendEmployeeNameBy(weekendIndex));
             }
             workDay.nextDay();
         }
+    }
+
+    private void printWeekendScheduleFormat(WorkDay workDay, String name) {
+        System.out.printf("%d월 %d일 %s %s", workDay.getMonth(), workDay.getDay(), workDay.getDayOfWeekByKorean(), name);
+        weekendIndex++;
+        printNewLine();
+    }
+
+    private void printWeekDayScheduleFormat(WorkDay workDay, String name) {
+        System.out.printf("%d월 %d일 %s %s", workDay.getMonth(), workDay.getDay(), workDay.getDayOfWeekByKorean(), name);
+        weekdayIndex++;
+        printNewLine();
+    }
+
+    private void printHolidayScheduleFormat(WorkDay workDay, String name) {
+        System.out.printf("%d월 %d일 %s (휴일) %s", workDay.getMonth(), workDay.getDay(),
+                workDay.getDayOfWeekByKorean(), name);
+        weekendIndex++;
+        printNewLine();
     }
 
     public void printNewLine() {
