4853c96c697384fb98292a700830d05c90823510
Hyeon0208 <dmsgktn0208@gmail.com>
2023-12-16 14:35:57 +0900

    feat : 잘못된 값 입력시 에러 메시지 출력 후 재입력 기능 추가

---
+++ src/main/java/oncall/view/handler/InputHandler.java
@@ -0,0 +1,29 @@
+package oncall.view.handler;
+
+import java.util.function.Supplier;
+import oncall.view.ErrorView;
+import oncall.view.InputView;
+
+public class InputHandler {
+    private final InputView inputView;
+    private final ErrorView errorView;
+
+    public InputHandler(InputView inputView, ErrorView errorView) {
+        this.inputView = inputView;
+        this.errorView = errorView;
+    }
+
+    public String receiveValidatedStartWorkDay() {
+        return receiveValidatedInput(inputView::inputStartWorkDay);
+    }
+
+    private <T> T receiveValidatedInput(Supplier<T> inputView) {
+        while (true) {
+            try {
+                return inputView.get();
+            } catch (IllegalArgumentException exception) {
+                errorView.printErrorMessage(exception.getMessage());
+            }
+        }
+    }
+}
